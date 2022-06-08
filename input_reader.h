2d76168a7e32984443a1ff012ecc7c3acde9384f
Konstantin Molibozhenko <rjcnz.igrok@yandex.ru>
2022-06-08 13:14:25 +0300

    Дабавлен класс InputReader;

--- input_reader.h
+++ input_reader.h
@@ -1,2 +1,51 @@
-// напишите решение с нуля
-// код сохраните в свой git-репозиторий
+#include <istream>
+#include <sstream>
+#include <map>
+#include <iostream>
+#include <set>
+#include <utility>
+#include <vector>
+
+class InputReader {
+public:
+    explicit InputReader(int number_requests) : request_queue_() {
+        for (int i = 0; i < number_requests; ++i) {
+            std::string line;
+            std::getline(std::cin, line);
+            Request request;
+            request.request_ = line;
+            if (line[0] == 'B') {
+                line = line.substr(line.find_first_not_of(' ', 3));
+
+                request_queue_[RequestType::BUS].push_back(line);
+            }
+            if (line[0] == 'S') {
+                line = line.substr(line.find_first_not_of(' ', 4));
+                request_queue_[RequestType::STOP].push_back(line);
+            }
+        }
+    }
+
+    void RequestProcessing() {
+
+    }
+
+
+private:
+    enum class RequestType {
+        BUS,
+        STOP,
+    };
+
+    struct Request {
+        Request() = default;
+
+        Request(std::string &request) : request_(std::move(request)) {
+        }
+
+        std::string request_;
+    };
+
+    std::map<RequestType, std::vector<Request>> request_queue_;
+
+};
--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
-// напишите решение с нуля
-// код сохраните в свой git-репозиторий
+#include "input_reader.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number_requests;
+    cin >> number_requests;
+    string hh;
+    getline(cin, hh);
+   InputReader input(number_requests);
+
+}
